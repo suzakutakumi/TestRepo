9461f27f4bbd25469c9b4fcd8689a1885e48ec47
suzakutakumi <suzakutakumi3@gmail.com>
2022-06-02 11:17:05 +0900

    Basic認証

---
+++ BasicAuth/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	auth := r.Group("/", gin.BasicAuth(gin.Accounts{
+		"suzaku": "pass",
+		"nemui":  "donabe",
+	}))
+	auth.GET("/", func(c *gin.Context) {
+		user := c.MustGet(gin.AuthUserKey).(string)
+		c.JSON(http.StatusOK, gin.H{"message": "Hello " + user})
+	})
+	r.Run(":8080")
+}
